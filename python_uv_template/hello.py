06127f502f0af0575a4d8e71dc92608dc7cb99d5
Nikhil Gupta <mywork.ng@gmail.com>
2025-04-15 16:06:44 -0500

    chore: revert

--- python_uv_template/hello.py
+++ python_uv_template/hello.py
@@ -10,17 +10,5 @@ def main() -> str:
     return return_value
 
 
-def untested_block():
-    """This block is not tested to demonstrate code coverage.
-
-    Returns
-    -------
-    str
-        Return a dummy value
-    """
-    return_value = "Hello, world!"
-    return return_value
-
-
 if __name__ == "__main__":
     print(main())
